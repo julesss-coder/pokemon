071463da263cbb320be6724f70398f0ab71e93b3
Julia <67640854+julesss-coder@users.noreply.github.com>
2023-02-17 12:10:19 +0100

    Added PokemonGameTest

---
+++ src/test/java/PokemonGameTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class PokemonGameTest {
+    private PokemonGame game;
+
+    @BeforeEach
+    void setUp() {
+        PokemonGame game = new PokemonGame();
+    }
+
+    /*
+    TEST:
+    - readPokemonFile() fills pokedex with appropriate amount of elements
+    - playerChoosesPokemon() assigns CORRECT pokemon to `playerPokemon`
+    - `computerChoosesPokemon()` assigns CORRECT pokemon to `computerPokemon`
+
+
+    - TESTS for exceptions
+    - only test for exceptions that you wrote yourself!
+     */
+
+
+
+}
